107c9418bba30c119c6f1decb63c0755f168497a
Anoop <Anooppopziyan@gmail.com>
2026-03-03 11:55:56 +0530

    Find the maximum & minimum number in an array of integers.

--- main.java
+++ main.java
@@ -2,20 +2,23 @@ import java.util.Scanner;
 public class main{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of names you want to input:");
-        int n = sc.nextInt();
-        String[]  names = new String[n];
-        System.out.println("Enter the names:");
-        for(int i = 0; i< n ;i++){
-            names[i] = sc.next();
-
+        System.out.println("Enter the number of elements in the array:");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        System.out.println("Enter the elements of the array:");
+        for(int i=0; i<size; i++){
+            arr[i] = sc.nextInt();
+            if(arr[i] <min){
+                min = arr[i];
+            }
+            if(arr[i] > max){
+                max = arr[i];
+            }
         }
-        System.out.println("The names you entered are:");
-
-        for(int i = 0; i< n ;i++){
-            
-            System.out.println(names[i]);
-        }
+        System.out.println("The minimum element in the array is: " + min);
+        System.out.println("The maximum element in the array is: " + max);
 
     }
 }
