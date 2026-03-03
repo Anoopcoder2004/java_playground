94662da7521f087cb7feb5bce3d1e0c828691347
Anoop <Anooppopziyan@gmail.com>
2026-03-03 17:26:18 +0530

    search number from 2d array

--- main.java
+++ main.java
@@ -1,24 +1,30 @@
 import java.util.*;
-public class main{
+
+public class main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number of rows and columns:");
         int rows = sc.nextInt();
         int cols = sc.nextInt();
-        int numbers[][] = new int [rows][cols];
-        System.out.println("Enter "+(rows*cols) + " numbers");
-        //input
-        for(int i = 0;i < rows; i++){
-            for(int j = 0;j< cols;j++){
+        int numbers[][] = new int[rows][cols];
+
+        System.out.println("Enter " + (rows * cols) + " numbers");
+        // input
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
                 numbers[i][j] = sc.nextInt();
             }
         }
-        System.out.println("Entered number is :");
+        System.out.println("Enter the number to find :");
+        int target = sc.nextInt();
+        System.out.println("Entered number is at index :");
         // output
-         for(int i = 0;i < rows; i++){
-            for(int j = 0;j< cols;j++){
-                System.out.print(numbers[i][j] + " ");
-            }System.out.println();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (numbers[i][j] == target) {
+                    System.out.println("number found at index:"+i+","+j);
+                }
             }
+        }
     }
 }
