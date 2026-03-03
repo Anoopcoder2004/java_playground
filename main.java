cc26ad88755c6090f4b00079379987e34990e3cd
Anoop <Anooppopziyan@gmail.com>
2026-03-03 12:04:28 +0530

    Take an array of numbers as input and check if it is an array sorted in ascending order.

--- main.java
+++ main.java
@@ -1,24 +1,20 @@
 import java.util.Scanner;
 public class main{
-    public static void main(String args[]){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of elements in the array:");
-        int size = sc.nextInt();
-        int[] arr = new int[size];
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        System.out.println("Enter the elements of the array:");
-        for(int i=0; i<size; i++){
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter numbers in ascending order:");
+        for(int i = 0; i<n; i++){
             arr[i] = sc.nextInt();
-            if(arr[i] <min){
-                min = arr[i];
-            }
-            if(arr[i] > max){
-                max = arr[i];
+        }
+        for(int i = 0; i< n-1; i++){
+            if(arr[i] > arr[i+1]){
+                System.out.println("The array is not in ascending order.");
+                return;
             }
         }
-        System.out.println("The minimum element in the array is: " + min);
-        System.out.println("The maximum element in the array is: " + max);
-
+        System.out.println("The array is in ascending order.");
     }
 }
