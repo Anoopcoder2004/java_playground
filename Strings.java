f5cb718718a467f5c5b4d403df7d2680226de994
Anoop <Anooppopziyan@gmail.com>
2026-03-04 16:26:32 +0530

    Input an email from the user. You have to create a username from the email by deleting the part that comes after ‘@’. Display that username to the user.

--- Strings.java
+++ Strings.java
@@ -3,17 +3,17 @@ import java.util.*;
 public class Strings {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter string:");
+        System.out.println("Enter email:");
         String word = sc.nextLine();
-        String result = "";
+        String userName = "";
         for(int i = 0; i<word.length();i++){
-            if(word.charAt(i)=='e'){
-                result+="i";
+            if(word.charAt(i)=='@'){
+                System.out.println(userName);
+                return;
             }else{
-                result+=word.charAt(i);
+                userName+=word.charAt(i);
             }
         }
-        System.out.println(result);
 
     }
 }
