cdfbdc0698bd54fe81c04b74a564d098f5d84c08
Anoop <Anooppopziyan@gmail.com>
2026-03-04 16:01:40 +0530

    Input a string from the user. Create a new string called ‘result’ in which you will replace the letter ‘e’ in the original string with letter ‘i’.

--- Strings.java
+++ Strings.java
@@ -2,14 +2,18 @@ import java.util.*;
 
 public class Strings {
     public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-       System.out.println("Enter the size");
-       int size = sc.nextInt();
-       int totalLength= 0;
-       for (int i = 0; i < size ; i++){
-            totalLength +=sc.next().length();
-       }
-       System.out.println(totalLength);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter string:");
+        String word = sc.nextLine();
+        String result = "";
+        for(int i = 0; i<word.length();i++){
+            if(word.charAt(i)=='e'){
+                result+="i";
+            }else{
+                result+=word.charAt(i);
+            }
+        }
+        System.out.println(result);
 
     }
 }
