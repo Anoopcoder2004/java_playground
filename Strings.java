4fc4215982a32c0e033869db045d5cbd7220ebd6
Anoop <Anooppopziyan@gmail.com>
2026-03-05 10:25:37 +0530

    reverse a string

--- Strings.java
+++ Strings.java
@@ -2,18 +2,21 @@ import java.util.*;
 
 public class Strings {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter email:");
-        String word = sc.nextLine();
-        String userName = "";
-        for(int i = 0; i<word.length();i++){
-            if(word.charAt(i)=='@'){
-                System.out.println(userName);
-                return;
-            }else{
-                userName+=word.charAt(i);
-            }
+        StringBuilder  name = new StringBuilder("ANAS");
+        System.out.println(name);
+        for(int i = 0; i<name.length()/2;i++){
+            int front = i;
+            int back = name.length() - 1 - i;
+            
+            char frontChar = name.charAt(front);
+            char backChar = name.charAt(back);
+            
+            name.setCharAt(front, backChar);
+            name.setCharAt(back, frontChar);
+
         }
+                System.out.println(name);
+
 
     }
 }
