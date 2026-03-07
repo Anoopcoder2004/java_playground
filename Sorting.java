567e8c049d5f6119867392b48cacf2a4be71fa3d
Anoop <Anooppopziyan@gmail.com>
2026-03-07 12:56:52 +0530

    insertion sort

--- Sorting.java
+++ Sorting.java
@@ -2,18 +2,17 @@ public class Sorting{
     public static void main(String args[]){
         int arr[] = {8,7,5,2,1,3};
 
-        //selection sort 
-        for(int i = 0;i<arr.length-1;i++){
-            int smallest = i;
-            for(int j = i+1;j<arr.length;j++){
-                if(arr[smallest]>arr[j]){
-                    smallest = j;
-                }
-            }
-            int temp = arr[smallest];
-            arr[smallest] = arr[i];
-            arr[i]= temp;
+        //insertion sort 
+       for(int i = 1;i<arr.length; i ++){
+        int current = arr[i];
+        int j = i-1;
+        while(j >=0 && current < arr[j]){
+            arr[j+1] = arr[j];
+            j--;
         }
+        // placement
+        arr[j+1]= current;
+       }
         printArray(arr);
         
     }
