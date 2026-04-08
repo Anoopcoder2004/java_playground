dffbffbbbc8d3b9f5ec556b71972b3d6b48cc010
Anoop <Anooppopziyan@gmail.com>
2026-04-08 14:57:10 +0530

    create project

---
+++ vivara-kendram/src/main/java/com/example/vivara/kendram/VivaraKendramApplication.java
@@ -0,0 +1,13 @@
+package com.example.vivara.kendram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VivaraKendramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VivaraKendramApplication.class, args);
+	}
+
+}
---
+++ vivara-kendram/src/test/java/com/example/vivara/kendram/VivaraKendramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vivara.kendram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VivaraKendramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
